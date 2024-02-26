e39c8e669e5d201817002153ccaea13d349a6de8
Pan Odin S <19510452+PanAchy@users.noreply.github.com>
2024-02-26 10:58:18 -0600

    Created checkWinner and playGame functions to complete assignment. Game is now functional.

--- scripts.js
+++ scripts.js
@@ -18,13 +18,63 @@ function getPlayerChoice(){
 }
 }
 
+function checkWinner(computer, player){
+    // ROCK  v SCISSORS --> ROCK
+    // ROCK  v PAPER    --> PAPER
+    // PAPER v SCISSORS --> SCISSORS
+    // TIE   v TIE      --> TIE
+    if (computer == "rock" && player == "scissors"){
+        return("C")
+    }
+    else if (computer == "rock" && player == "paper"){
+        return("P")
+    }
+    else if (computer == "paper" && player == "scissors"){
+        return ("P")
+    }
+    else if (computer == "scissors" && player == "rock"){
+        return ("P")
+    }
+    else if (computer == "paper" && player == "rock"){
+        return ("C")
+    }
+    else if (computer == "scissors" && player == "paper"){
+        return ("C")
+    }
+    else if (computer == player){
+        return ("T")
+    }
+}
 function playGame(){
-    
+    let ComputerScore = 0;
+    let PlayerScore = 0;
+    while (ComputerScore < 5 && PlayerScore < 5){
+        if (checkWinner(getComputerChoice(),getPlayerChoice()) == "C"){
+            console.log("C")
+            ComputerScore++
+        }
+        else if (checkWinner(getComputerChoice(),getPlayerChoice()) == "P"){
+            console.log("P")
+            PlayerScore++
+        }
+        else {
+            console.log("T")
+            continue
+        }
+        console.log(`Current score is ${ComputerScore} for computer and ${PlayerScore} for player.`)
+    }
+    if (ComputerScore == 5){
+        console.log("Computer wins!")
+    }
+    else if (PlayerScore == 5){
+        console.log("Player wins!")
+    }
 }
 
 const options = ["rock","paper","scissors"]
 
+
 // const computerSelection = getComputerChoice()
 // const playerSelection = getPlayerChoice()
 
-console.log(getPlayerChoice())
+console.log(playGame())
